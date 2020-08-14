98ab988916261a1016fb51bcaee29320085c505a
Saurabh Burade <sburade3@gmail.com>
2020-08-14 17:32:08 +0530

    refactor - controllers

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,1 +1,120 @@
-const login = 
+const User = require("../models/users.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const register = (req, res) => {
+    const {fname, lname, email, password} = req.body;
+    console.log(req.body);
+    const hash = bcrypt.hashSync(password, 10);
+    const token = jwt.sign(email, process.env.JWT_SECRET);
+    console.log("hash", hash);
+    console.log("jwt", token);
+    User.findOne({email})
+        .then(doc => {
+            if (doc) {
+                res.status(400).json({Error: "Already Exist"});
+            } else {
+                const newUser = new User({
+                    fname,
+                    lname,
+                    token,
+                    email,
+                    password: hash,
+                });
+                console.log(newUser);
+                newUser
+                    .save()
+                    .then(user => {
+                        user.password = "";
+                        res.status(200).json(user);
+                        console.log(user);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(400).json(err);
+                    });
+            }
+        })
+        .catch(err => {
+            console.log("err", err);
+            res.status(400).json(err);
+        });
+};
+
+const login = (req, res) => {
+    const {email, password} = req.body;
+    User.findOne({email})
+        .then(doc => {
+            console.log("dc", doc);
+            if (doc) {
+                const match = bcrypt.compareSync(password, doc.password);
+                if (match) {
+                    doc.password = "";
+                    res.status(200).json(doc);
+                } else {
+                    res.status(400).json({Error: "Incorrect Password"});
+                }
+            } else {
+                res.status(404).json({Error: "User Not Found"});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json({Error: "Something went wrong"});
+        });
+}
+
+
+const allUsers = (req, res) => {
+    User.find()
+        .then(docs => {
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+};
+const fetchUser = (req, res) => {
+    const {token} = req.headers;
+    User.findOne({token})
+        .then(doc => {
+            if (doc) {
+                doc.password = "";
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({Error: "User Not Found"});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json({Error: "Something went wrong"});
+        });
+};
+const addChat = (req, res) => {
+    const {token} = req.headers;
+    const {chat} = req.body;
+    User.findOne({token})
+        .then(doc => {
+            if (doc) {
+                // console.log(doc);
+                console.log(doc.chats.includes(chat));
+                if (!doc.chats.includes(chat)) {
+                    doc.chats = [...doc.chats, chat];
+
+                    doc.save().then(user => {
+                        doc.password = "";
+                        res.status(200).json(doc.chats);
+                    });
+                } else {
+                    res.status(400).json({Error: "Already in chat list"});
+                }
+            } else {
+                res.status(404).json({Error: "User Not Found"});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json({Error: "Something went wrong"});
+        });
+};
+module.exports = {register, login, allUsers, fetchUser, addChat};
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,131 +1,30 @@
 const router = require("express").Router();
-const User = require("../models/users.model");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+
 const verifyJwt = require("./jwt.middleware");
+const {
+    login,
+    register,
+    allUsers,
+    fetchUser,
+    addChat,
+} = require("../controllers/user.controllers");
 
 // Fetch all users
-router.get("/all-users", (req, res) => {
-    User.find()
-        .then(docs => {
-            res.status(200).json(docs);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
+router.get("/all-users", allUsers);
+
 //register user
-router.post("/register", (req, res) => {
-    const {fname, lname, email, password} = req.body;
-    console.log(req.body);
-    const hash = bcrypt.hashSync(password, 10);
-    const token = jwt.sign(email, process.env.JWT_SECRET);
-    console.log("hash", hash);
-    console.log("jwt", token);
-    User.findOne({email})
-        .then(doc => {
-            if (doc) {
-                res.status(400).json({Error: "Already Exist"});
-            } else {
-                const newUser = new User({
-                    fname,
-                    lname,
-                    token,
-                    email,
-                    password: hash,
-                });
-                console.log(newUser);
-                newUser
-                    .save()
-                    .then(user => {
-                        user.password = "";
-                        res.status(200).json(user);
-                        console.log(user);
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        res.status(400).json(err);
-                    });
-            }
-        })
-        .catch(err => {
-            console.log("err", err);
-            res.status(400).json(err);
-        });
-});
+router.post("/register", register);
 
 // Login user
-router.post("/login", (req, res) => {
-    const {email, password} = req.body;
-    User.findOne({email})
-        .then(doc => {
-            console.log("dc", doc);
-            if (doc) {
-                const match = bcrypt.compareSync(password, doc.password);
-                if (match) {
-                    doc.password = "";
-                    res.status(200).json(doc);
-                } else {
-                    res.status(400).json({Error: "Incorrect Password"});
-                }
-            } else {
-                res.status(404).json({Error: "User Not Found"});
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(400).json({Error: "Something went wrong"});
-        });
-});
+router.post("/login", login);
+
+//jwt verification middleware
+router.use(verifyJwt);
 
 // fetch profile
-router.use(verifyJwt);
-router.get("/user", (req, res) => {
-    const {token} = req.headers;
-    User.findOne({token})
-        .then(doc => {
-            if (doc) {
-                doc.password = "";
-                res.status(200).json(doc);
-            } else {
-                res.status(404).json({Error: "User Not Found"});
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(400).json({Error: "Something went wrong"});
-        });
-});
-
-//user profile update
+router.get("/user", fetchUser);
 
 // add chats
-router.post("/chat/add", (req, res) => {
-    const {token} = req.headers;
-    const {chat} = req.body;
-    User.findOne({token})
-        .then(doc => {
-            if (doc) {
-                // console.log(doc);
-                console.log(doc.chats.includes(chat));
-                if (!doc.chats.includes(chat)) {
-                    doc.chats = [...doc.chats, chat];
-
-                    doc.save().then(user => {
-                        doc.password = "";
-                        res.status(200).json(doc.chats);
-                    });
-                } else {
-                    res.status(400).json({Error: "Already in chat list"});
-                }
-            } else {
-                res.status(404).json({Error: "User Not Found"});
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(400).json({Error: "Something went wrong"});
-        });
-});
+router.post("/chat/add", addChat);
 
 module.exports = router;
