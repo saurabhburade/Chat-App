222c13423107fc6d9802dd056267813b6f00d0d9
Saurabh Burade <sburade3@gmail.com>
2020-08-14 16:08:13 +0530

    Add - User register,login,fetch APIs

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,6 +3,7 @@ const User = require("../models/users.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+// Fetch all users
 router.get("/all-users", (req, res) => {
     User.find()
         .then(docs => {
@@ -12,7 +13,7 @@ router.get("/all-users", (req, res) => {
             res.status(400).json(err);
         });
 });
-
+//register user
 router.post("/register", (req, res) => {
     const {fname, lname, email, password} = req.body;
     console.log(req.body);
@@ -36,6 +37,7 @@ router.post("/register", (req, res) => {
                 newUser
                     .save()
                     .then(user => {
+                        doc.password = "";
                         res.status(200).json(user);
                         console.log(user);
                     })
@@ -49,8 +51,61 @@ router.post("/register", (req, res) => {
             console.log("err", err);
             res.status(400).json(err);
         });
-
-    // const newUser
 });
+
+// Login user
+router.post("/login", (req, res) => {
+    const {email, password} = req.body;
+    User.findOne({email})
+        .then(doc => {
+            console.log("dc", doc);
+            if (doc) {
+                const match = bcrypt.compareSync(password, doc.password);
+                if (match) {
+                    doc.password = "";
+                    res.status(200).json(doc);
+                } else {
+                    res.status(400).json({Error: "Incorrect Password"});
+                }
+            } else {
+                res.status(404).json({Error: "User Not Found"});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json({Error: "Something went wrong"});
+        });
+});
+
+// fetch profile
+
+router.get("/user", (req, res) => {
+    const {token} = req.headers;
+
+    //verify jwt
+    jwt.verify(token, process.env.JWT_SECRET, (err, verifiedJwt) => {
+        console.log(err, verifiedJwt);
+        if (err) {
+            res.status(400).json({Error: "Invalid Token"});
+        } else {
+            User.findOne({token})
+                .then(doc => {
+                    if (doc) {
+                        doc.password = "";
+                        res.status(200).json(doc);
+                    } else {
+                        res.status(404).json({Error: "User Not Found"});
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(400).json({Error: "Something went wrong"});
+                });
+        }
+    });
+});
+
+
+
 
 module.exports = router;
