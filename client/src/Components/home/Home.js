fbc94d06f5a64f27fb4de54355fb0b4409e505d4
Saurabh Burade <sburade3@gmail.com>
2020-08-23 16:45:19 +0530

    added homepage animations

--- client/src/Components/home/Home.js
+++ client/src/Components/home/Home.js
@@ -15,12 +15,14 @@ import bootstrapIcon from "../../assets/bootstrap.svg";
 import antIcon from "../../assets/ant.svg";
 import firebaseIcon from "../../assets/firebase.svg";
 import {GithubOutlined} from "@ant-design/icons";
+import WOW from "wowjs"
+new WOW.WOW().init();
 const {Step} = Steps;
 function Home() {
     return (
         <div className="home-container">
             <div className="hero-container ">
-                <div className="hero-text">
+                <div className="hero-text wow animate__animated animate__fadeInUp   ">
                     <p> Welcome to Chat-App</p>
                     <p>
                         Lets get started with chatapp and connect with your
@@ -45,13 +47,13 @@ function Home() {
                         )}
                     </div>
                 </div>
-                <div className="hero-image-cont">
+                <div className="hero-image-cont wow animate__animated animate__fadeInUp">
                     <img className="hero-image" src={image} alt="" />
                 </div>
             </div>
             <div className="steps">
                 <h3>Steps</h3>
-                <div className="d-flex align-items-center">
+                <div className="d-flex align-items-center wow animate__animated animate__fadeInUp">
                     <Steps progressDot direction="vertical">
                         <Step
                             status="finish"
@@ -99,57 +101,63 @@ function Home() {
                     /> */}
                     </Steps>
                     <div>
-                        <img className="chat-screen" src={chatimage} alt="" />
+                        <img
+                            className="chat-screen wow  animate__animated animate__fadeInUp"
+                            src={chatimage}
+                            alt=""
+                        />
                     </div>
                 </div>
             </div>{" "}
             <div className="steps tech-cont">
                 <h3>Technologies Used</h3>
-                <div className="d-flex align-items-center justify-content-center">
+                <div className="d-flex align-items-center justify-content-center wow  animate__animated animate__fadeInUpwow  animate__animated animate__fadeInUp">
                     <Tooltip placement="bottom" title="ReactJS">
-                        <div className="tech-icons-cont">
+                        <div className="tech-icons-cont wow  animate__animated animate__fadeInUp">
                             <img src={reactIcon} alt="" />
                         </div>
                     </Tooltip>
                     <Tooltip placement="bottom" title="Redux">
-                        <div className="tech-icons-cont">
+                        <div className="tech-icons-cont wow  animate__animated animate__fadeInUp">
                             <img src={reduxIcon} alt="reduxIcon" />
                         </div>
                     </Tooltip>
                     <Tooltip placement="bottom" title="NodeJS">
-                        <div className="tech-icons-cont">
+                        <div className="tech-icons-cont wow  animate__animated animate__fadeInUp">
                             <img src={nodeIcon} alt="nodeIcon" />
                         </div>
                     </Tooltip>
                     <Tooltip placement="bottom" title="ExpressJS">
-                        <div className="tech-icons-cont">
+                        <div className="tech-icons-cont wow  animate__animated animate__fadeInUp">
                             <img src={expreeIcon} alt="expreeIcon" />
                         </div>
                     </Tooltip>
                     <Tooltip placement="bottom" title="MongoDB">
-                        <div className="tech-icons-cont">
+                        <div className="tech-icons-cont wow  animate__animated animate__fadeInUp">
                             <img src={mongoIcon} alt="mongoIcon" />
                         </div>
                     </Tooltip>
                     <Tooltip placement="bottom" title="Firebase">
-                        <div className="tech-icons-cont">
+                        <div className="tech-icons-cont wow  animate__animated animate__fadeInUp">
                             <img src={firebaseIcon} alt="antIcon" />
                         </div>
                     </Tooltip>
                     <Tooltip placement="bottom" title="Bootstrap">
-                        <div className="tech-icons-cont">
+                        <div className="tech-icons-cont wow  animate__animated animate__fadeInUp">
                             <img src={bootstrapIcon} alt="bootstrapIcon" />
                         </div>
                     </Tooltip>
                     <Tooltip placement="bottom" title="Ant-Design">
-                        <div className="tech-icons-cont">
+                        <div className="tech-icons-cont wow  animate__animated animate__fadeInUp">
                             <img src={antIcon} alt="antIcon" />
                         </div>
                     </Tooltip>
                 </div>
             </div>{" "}
             <div className="steps tech-cont ">
-                <h3 className="pb-0">About Project</h3>
+                <h3 className="pb-0 wow  animate__animated animate__fadeInUp">
+                    About Project
+                </h3>
                 <p>This Project is developed by Saurabh Burade</p>
                 <div className="d-flex align-items-center justify-content-center">
                     <a
@@ -168,15 +176,6 @@ function Home() {
                     </a>
                 </div>
             </div>
-            {/* import reactIcon from "../../assets/reactjs.svg" import reduxIcon
-            from "../../assets/redux.svg"; import nodeIcon from
-            "../../assets/nodejs.svg"; import expreeIcon from
-            "../../assets/express.svg"; import mongoIcon from
-            "../../assets/mongo.svg"; import bootstrapIcon from
-            "../../assets/bootstrap.svg"; import antIcon from
-            "../../assets/ant.svg"; */}
-            {/* <h1>Hello</h1>
-            <img className="hero-image" src={image} alt="" /> */}
         </div>
     );
 }
